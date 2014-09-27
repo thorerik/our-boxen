efd8daa77da18d8c7c9a65adc32bec18870ca72e
Thor Erik Lie <thor@thorerik.com>
2014-09-27 11:07:27 +0200

    adjuested configuration

--- modules/people/manifests/thorerik.pp
+++ modules/people/manifests/thorerik.pp
@@ -1,5 +1,7 @@
 class people::thorerik {
-    
+
+    notify { 'class people::thorerik declared': }
+
     include osx::global::disable_key_press_and_hold
     include osx::global::enable_keyboard_control_access
     include osx::global::expand_print_dialog
@@ -16,6 +18,30 @@ class people::thorerik {
     include osx::software_update
     include osx::keyboard::capslock_to_control
 
+    include python
+    include zsh
+    include firefox
+    include tmux
+    include vmware_fusion
+    include phpstorm
+    include better_touch_tools
+    include chrome
+    include java
+    include iterm2::dev
+    include dropbox
+    include alfred
+    include skype
+    include spotify
+    include steam
+    include nmap
+    include sublime_text
+    include atom
+    include wget
+    include crashplan
+    include skydrive
+    include caffeine
+    include textmate
+    include teamviewer
 
 
     $home     = "/Users/thor"
@@ -25,12 +51,29 @@ class people::thorerik {
     file { $my:
         ensure  => directory
     }
-
     repository { $dotfiles:
         source  => 'thorerik/dotFiles',
         require => File[$my]
     }
 
+    exec { "install-spf13-vim":
+        command => "curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh",
+        creates => "${home}/.vimrc",
+    }
 
+    exec { "install-oh-my-zsh":
+        command => "git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh",
+        creates => "${home}/.oh-my-zsh",
+    }
+
+    exec { "link-zshrc":
+        command => "ln -s ${dotfiles}/zshrc ${home}/.zshrc",
+        creates => "${home}/.zshrc",
+    }
+
+    exec { "link-tmux.conf":
+        command => "ln -s ${dotfiles}/tmux.conf ${home}/.tmux.conf",
+        creates => "${home}/.tmux.conf"
+    }
 
 }
